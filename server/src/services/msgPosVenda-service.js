5db8ab8287aca188c86dccfef2f676e9591f4d18
Felipe Miguel Dos Santos <felipe.msantos@gruposaga.com.br>
2020-06-17 15:34:35 -0300

    Deploy

--- server/src/services/msgPosVenda-service.js
+++ server/src/services/msgPosVenda-service.js
@@ -3,7 +3,22 @@ const axios = require('axios')
 const MensagemPosVenda = require("../models/mensagemPosVenda-model")
 
 const salvarBd = async (req, res) => {
-    let mensagemPosVenda = new MensagemPosVenda(req.body)    
+    MensagemPosVenda.findOneAndUpdate({ user_id: req.body.userId }, {
+        $set: {
+            isHabilitarEnvioCompraRealizada: req.body.isHabilitarEnvioCompraRealizada,
+            mensagemCompraRealizada: req.body.mensagemCompraRealizada,
+            isHabilitarEnvioProdutoEmTransito: req.body.isHabilitarEnvioProdutoEmTransito,
+            mensagemProdutoEmTransito: req.body.mensagemProdutoEmTransito,
+            isHabilitarEnvioProntoParaRetirarCorreios: req.body.isHabilitarEnvioProntoParaRetirarCorreios,
+            mensagemProntoParaRetirarCorreios: req.body.mensagemProntoParaRetirarCorreios,
+            isHabilitarEnvioProdutoEntregue: req.body.isHabilitarEnvioProdutoEntregue,
+            mensagemProdutoEntregue: req.body.mensagemProdutoEntregue,
+        }
+    }).then(response => {
+        res.send("Atualizado")
+    }).catch(err => res.send(err))
+
+    let mensagemPosVenda = new MensagemPosVenda(req.body)
     await mensagemPosVenda.save().then(response => {
         console.info("[MENSAGEM DO SISTEMA] - Mensagem de pos venda salvo no banco de dados!")
         res.status(200).send("OK")
